93e9d4221b89f7b39c015107fcc5de0598adf34f
Manuel Rueda <rueda.manu@gmail.com>
2025-03-05 13:23:41 +0100

    Source commit

---
+++ examples/run_mit_cohort.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+set -eu 
+
+dir=/media/mrueda/2TBS/CNAG/Project_CBI_Call/
+scrippscall=/media/mrueda/2TBS/CNAG/Project_CBI_Call/scrippscall/bin/scrippscall
+ncpu=4
+
+for dirname in MA99999_exome
+do
+ cd $dir/$dirname
+ echo $dirname
+  echo "...$dirname"
+  cat<<EOF> $dirname.mit_cohort.in
+mode            cohort
+pipeline        mit
+sample          $dir/$dirname
+EOF
+
+$scrippscall -t $ncpu -i $dirname.mit_cohort.in 
+cd ..
+done
--- examples/run_mit_single.sh
+++ examples/run_mit_single.sh
@@ -5,7 +5,7 @@ dir=/media/mrueda/2TBS/CNAG/Project_CBI_Call/
 scrippscall=/media/mrueda/2TBS/CNAG/Project_CBI_Call/scrippscall/bin/scrippscall
 ncpu=4
 
-for dirname in MA00001_exome
+for dirname in MA99999_exome
 do
  cd $dir/$dirname
  echo $dirname
@@ -13,12 +13,12 @@ do
  do
   echo "...$sample"
   cd $sample
-  cat<<EOF>$sample.wes_single.in
+  cat<<EOF>$sample.mit_single.in
 mode            single
-pipeline        wes
+pipeline        mit
 sample          $dir/$dirname/$sample
 EOF
-$scrippscall -t $ncpu -i $sample.wes_single.in > $sample.wes_single.log 2>&1
+$scrippscall -t $ncpu -i $sample.mit_single.in > $sample.mit_single.log 2>&1
   cd ..
  done
 cd ..
